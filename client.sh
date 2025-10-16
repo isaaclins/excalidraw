6c544b6e52b90433f124dd746ad81cc9134f290b
isaaclins <isaac.lins07@gmail.com>
2025-10-16 11:58:28 +0200

    Refactor project structure to create a new Tauri app named 'excaliapp'. Removed old app files and added new files including a client script, server script, and updated .gitignore. Introduced a new README for the Tauri app and set up necessary configurations for Tauri development.

---
+++ client.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Author: Isaac Lins
+# Description: Client script for the chat application
+# Usage: ./client.sh
+# Example: ./client.sh
+
+set -euo pipefail
+
+cd excaliapp && pnpm tauri dev
