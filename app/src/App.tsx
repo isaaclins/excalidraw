421516b76f4cba60a2528006b99a45f5b87c0f2d
Isaac Lins <contact@isaaclins.com>
2025-10-28 11:42:17 +0100

    feat: integrate MainMenu into Excalidraw component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,24 @@
 import '@excalidraw/excalidraw/index.css'
-import { Excalidraw } from '@excalidraw/excalidraw'
+import { Excalidraw, MainMenu } from '@excalidraw/excalidraw'
 import './App.css'
 
 function App() {
   return (
     <div className="app-shell">
-      <Excalidraw />
+      <Excalidraw>
+        <MainMenu>
+
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.Export />
+          <MainMenu.DefaultItems.CommandPalette />
+          <MainMenu.DefaultItems.SearchMenu />
+          <MainMenu.Separator />
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+          <MainMenu.DefaultItems.ToggleTheme />
+          <MainMenu.DefaultItems.Help />
+        </MainMenu>
+      </Excalidraw>
     </div>
   )
 }
